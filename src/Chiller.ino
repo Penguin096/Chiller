0e77806c48377f5a7d11717637939c8626a1a19b
Penguin096 <pavelshirobokov@yandex.ru>
2023-08-18 16:40:14 +0300

    fix

--- src/Chiller.ino
+++ src/Chiller.ino
@@ -7,8 +7,8 @@
 
 #define Pump_Off 60 // Задержка выключения помпы в сек.
 
-#define Pressure (11.0 - (-0.5))    //PAA-21Y 81556.11
-//#define Pressure (12.8 - (-1.0))  //BC-TP-013N
+//#define Pressure (11.0 - (-0.5))    //PAA-21Y 81556.11
+#define Pressure (12.8 - (-1.0))  //BC-TP-013N
 
 #define Fan_PWM_Low 128
 
